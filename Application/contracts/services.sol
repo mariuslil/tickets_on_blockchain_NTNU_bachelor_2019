51acfe74bd84f6fe982cac088c08d9a723345526
Andrine Celine Flatby <andrinef@stud.ntnu.no>
2019-04-21 17:45:58 +0200

    working the smart contract and starting to make the user-interface

---
+++ Application/contracts/services.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Services{
+    struct ticket{
+        uint ticket_id;
+        string name;
+        bool available;
+    }
+    
+    mapping(uint => ticket) tickets;
+    uint TicketsKey = 0;
+    uint maxTickets = 10; //testing
+    
+    function addTciket(uint _id, string memory _name, bool _available) public{
+        if(TicketsKey >= maxTickets){
+        tickets[TicketsKey] = ticket(_id, _name, _available);
+        TicketsKey++;
+        } 
+    }
+}

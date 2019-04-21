5b98764415bc85b6c170791d9ec7a50f106cd7c7
Andrine Celine Flatby <andrinef@stud.ntnu.no>
2019-04-21 18:28:05 +0200

    Fix the user-interface

--- Application/contracts/services.sol
+++ Application/contracts/services.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract Services{
+
     struct ticket{
         uint ticket_id;
         string name;
@@ -11,6 +12,7 @@ contract Services{
     uint TicketsKey = 0;
     uint maxTickets = 10; //testing
     
+    //add ticket to blockchain network
     function addTciket(uint _id, string memory _name, bool _available) public{
         if(TicketsKey >= maxTickets){
         tickets[TicketsKey] = ticket(_id, _name, _available);
@@ -18,7 +20,8 @@ contract Services{
         } 
     }
 
-    function getTicket() public{
-        
+    //getting the ticket by id from blockchainnetwork
+    function getTicket(uint _id) public view returns(uint, string memory, bool){
+
     }
 }

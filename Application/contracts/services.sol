09db6e90db35fafaf02b1dc1ed521c2f62c31cf5
Andrine Celine Flatby <andrinef@stud.ntnu.no>
2019-04-29 15:54:09 +0200

    starting to make the struc and function for games

--- Application/contracts/services.sol
+++ Application/contracts/services.sol
@@ -3,20 +3,34 @@ pragma solidity >=0.4.22 <0.6.0;
 contract Services{
 
     struct ticket{
-        uint id;
+        uint ticket_id;
+        string owner;
     }
 
+    /*
+    TODO: make score in game
+    */
+    struct game{
+        uint game_id;
+        string homeTeam;
+        string awayTeam;
+        uint number_of_tickets;
+    }
+    game[] Games;
     ticket[] Tickets;
 
-    function createTicket(uint _id) public{
-        //Ticket[_id].id=_id;
-        //Ticket[_id].events=_events;
-        ticket memory t = ticket(_id);
+    function createGame(uint _id, string memory _homeTeam, string memory _awayTeam, uint _tickets) public{
+        game memory g = game(_id, _homeTeam, _awayTeam, _tickets);
+        Games.push(g);
+    }
+
+    function createTicket(uint _id, string memory _owner) public{
+        ticket memory t = ticket(_id, _owner);
         Tickets.push(t);
     }
 
-    function getTicket(uint _id) public view returns(uint){
+    function getTicket(uint _id) public view returns(uint, string memory){
         ticket memory myTicket = Tickets[_id];
-        return(myTicket.id);
+        return(myTicket.ticket_id, myTicket.owner);
     }
 }

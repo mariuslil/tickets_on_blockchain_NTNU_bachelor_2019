18d3bc46f78b4216034d65f72c4fd03bcf1c5d54
Andrine Celine Flatby <andrinef@stud.ntnu.no>
2019-04-30 08:20:03 +0200

    add comments more for the code

--- Application/contracts/services.sol
+++ Application/contracts/services.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.4.22 <0.6.0;
 /* TODO:
 -Created a event for game and tickets
 -Make a score variable in struct game
+-Create a function for get all the available (true) ticket
+-Create a struct for a owner of a ticket
 */
 
 contract Services{
@@ -12,7 +14,7 @@ contract Services{
         uint ticket_id;
         //the id of game to the ticket
         uint game_id;
-        //whatever if ticket is available
+        //if ticket is available or not
         bool available;
     }
 
@@ -35,10 +37,11 @@ contract Services{
     //all Tickets
     ticket[] Tickets;
 
-    //Create a game in Game[] adn then create the number of ticket in the game in Tickets[]
+    //Create a game in Game[] and then create the number of ticket in the game in Tickets[]
     function createGame(uint _Gameid, string memory _homeTeam, string memory _foreignTeam, uint _tickets ) public{
         game memory g = game(_Gameid, _homeTeam, _foreignTeam, _tickets);
 
+        //Create number of ticket with _tickets in Tickets[], also include the same _Gameid as Games[] belong to
         for(uint i = 0; i <= g.number_of_tickets; i++){
             ticket memory t = ticket(i, _Gameid, true);
             Tickets.push(t);
